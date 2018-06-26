cf756ec326a80558e85a5e1b6e746411bd8cc84f
Fabulous_Muscles <disneylandcanwait@mail.ru>
2018-06-26 18:21:44 +0300

    Выполняет задание 5.2 личного проект

---
+++ js/form-file.js
@@ -0,0 +1,65 @@
+'use strict';
+(function () {
+  var inputValue = document.querySelector('.img-upload__text');
+  var textAreaValue = document.querySelector('.text__description');
+
+  var textAreaValueFocusHandler = function () {
+    document.removeEventListener('keydown', window.scaleFilter.popupKeydownHandler);
+  };
+
+  var textAreaValueBlurHandler = function () {
+    document.addEventListener('keydown', window.scaleFilter.popupKeydownHandler);
+  };
+
+  var isIdentically = function (element, array) {
+    var isTrue = false;
+    for (var i = 0; i < array.length; i++) {
+      if (element.toLowerCase() !== array[i].toLowerCase()) {
+        isTrue = true;
+      }
+    } return isTrue;
+  };
+
+  var isMoreThanOneHashtagSymbol = function (element) {
+    var isTrue = false;
+    var hashtagArrayFirstElement = element;
+    for (var i = 1; i < hashtagArrayFirstElement.length; i++) {
+      if (hashtagArrayFirstElement[i] !== '#') {
+        isTrue = true;
+      }
+    } return isTrue;
+  };
+
+  var textInputValueInputHandler = function (evt) {
+    if (evt.target.classList.contains('text__hashtags')) {
+      var hashtagArray = evt.target.value.split(' ');
+      for (var i = 0; i < hashtagArray.length; i++) {
+        if (hashtagArray.length > 5) {
+          evt.target.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
+        } else if (evt.target.value === '') {
+          evt.target.setCustomValidity('');
+        } else if (hashtagArray[i].indexOf(',') !== -1) {
+          evt.target.setCustomValidity('хэш-теги разделяются пробелами');
+        } else if (hashtagArray[i].indexOf('#') !== 0) {
+          evt.target.setCustomValidity('хэш-тег начинается с символа # (решётка)');
+        } else if (hashtagArray[i].length === 1 && hashtagArray[i].indexOf('#') === 0 || !isMoreThanOneHashtagSymbol(hashtagArray[i])) {
+          evt.target.setCustomValidity('хеш-тег не может содержать только решётку');
+        } else if (hashtagArray[i].length > 20) {
+          evt.target.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');
+        } else if (!isIdentically(hashtagArray[i], hashtagArray) && hashtagArray.length !== 1) {
+          evt.target.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');
+        } else {
+          evt.target.setCustomValidity('');
+        }
+      }
+    }
+    return hashtagArray;
+  };
+
+  inputValue.addEventListener('input', textInputValueInputHandler);
+
+  textAreaValue.addEventListener('focus', textAreaValueFocusHandler);
+
+  textAreaValue.addEventListener('blur', textAreaValueBlurHandler);
+
+})();
---
+++ js/preview.js
@@ -0,0 +1,62 @@
+'use strict';
+(function () {
+  var renderedPicture;
+  var elementComment = document.querySelectorAll('.social__comment');
+  var renderBigPicture = function (arrayObjects) {
+    var bigPictureElement = window.gallery.bigPictureBlock.cloneNode(true);
+    bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = arrayObjects.url;
+    bigPictureElement.querySelector('.likes-count').textContent = arrayObjects.likes;
+    bigPictureElement.querySelector('.comments-count').textContent = arrayObjects.comments.length;
+    bigPictureElement.querySelector('.social__caption').textContent = arrayObjects.description;
+    bigPictureElement.querySelector('.social__comment-count').classList.add('visually-hidden');
+    bigPictureElement.querySelector('.social__loadmore').classList.add('visually-hidden');
+    renderComments(arrayObjects);
+
+    return bigPictureElement;
+  };
+
+  var renderComments = function (arrayObjects) {
+    var elementCommentFragment = elementComment;
+    for (var i = 0; i < elementCommentFragment.length; i++) {
+      elementCommentFragment[i].querySelector('img').src = 'img/avatar-' + Math.floor(Math.random() * 6 + 1) + '.svg';
+      elementCommentFragment[i].querySelector('.social__text').textContent = arrayObjects.comments[i];
+    }
+    return elementCommentFragment;
+  };
+
+  var closeGallery = function () {
+    renderedPicture.classList.add('hidden');
+    document.removeEventListener('keydown', galleryKeydownHandler);
+  };
+
+  var galleryKeydownHandler = function (evt) {
+    if (evt.keyCode === window.dataFile.ESC_KEYCODE) {
+      closeGallery();
+    }
+  };
+
+  var renderedPictureClickHandler = function (evt) {
+    if (evt.target.classList.contains('big-picture__cancel')) {
+      closeGallery();
+    }
+  };
+
+  window.preview = {
+    pictureMainBlockHandler: function (evt) {
+      if (evt.target.classList.contains('picture__img')) {
+        var elementTargetImg = evt.target.src;
+        for (var i = 0; i < window.gallery.pictures.length; i++) {
+          if (elementTargetImg.includes(window.gallery.pictures[i].url)) {
+            window.gallery.bigPictureBlock.classList.remove('hidden');
+            renderBigPicture(window.gallery.pictures[i]);
+            renderedPicture = window.gallery.main.appendChild(renderBigPicture(window.gallery.pictures[i]));
+          }
+        }
+        document.addEventListener('keydown',galleryKeydownHandler);
+        renderedPicture.addEventListener('click',renderedPictureClickHandler);
+      }
+
+      return renderedPicture;
+    }
+  };
+})();
